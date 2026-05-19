4cf376f23c994ac905f362149cc1ba0a90d420f2
TBKALEB <kalebjesusuno1@gmail.com>
2026-05-19 16:35:14 -0600

    initial commit

---
+++ mi-3d-project/src/constants/index.js
@@ -0,0 +1,10 @@
+const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "Iphone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "AirPods" },
+];
+
+export { navLinks };
